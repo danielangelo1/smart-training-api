94c120c45e7df899207176d7c040ef0288c45ba5
danielangelo1 <danielangelo1234@gmail.com>
2026-03-11 21:04:12 -0300

    feat: add create-workout-plan endpoint

---
+++ src/useCases/CreateWorkoutPlan.ts
@@ -0,0 +1,87 @@
+import { NotFoundError } from "../errors/index.js";
+import { WeekDay } from "../generated/prisma/enums.js";
+import { prisma } from "../lib/db.js";
+
+interface InputDTO {
+  userId: string;
+  name: string;
+  workoutDays: Array<{
+    name: string;
+    weekDay: WeekDay;
+    isRest: boolean;
+    estimatedDurationInSeconds: number;
+    exercises: Array<{
+      name: string;
+      order: number;
+      sets: number;
+      reps: number;
+      restTimeInSeconds: number;
+    }>;
+  }>;
+}
+
+// export interface OutputDTO {
+//   id: string;
+// }
+
+class CreateWorkoutPlan {
+  async execute(dto: InputDTO) {
+    const existingWorkoutPlan = await prisma.workoutPlan.findFirst({
+      where: {
+        isActive: true,
+      },
+    });
+
+    // ACID Transaction if exists active workout plan
+    return prisma.$transaction(async (tx) => {
+      if (existingWorkoutPlan) {
+        await tx.workoutPlan.update({
+          where: { id: existingWorkoutPlan.id },
+          data: { isActive: false },
+        });
+      }
+
+      const workoutPlan = await tx.workoutPlan.create({
+        data: {
+          name: dto.name,
+          userId: dto.userId,
+          isActive: true,
+          workoutDays: {
+            create: dto.workoutDays.map((workoutDay) => ({
+              name: workoutDay.name,
+              weekDay: workoutDay.weekDay,
+              isRest: workoutDay.isRest,
+              estimatedDurationInSeconds: workoutDay.estimatedDurationInSeconds,
+              exercises: {
+                create: workoutDay.exercises.map((exercise) => ({
+                  name: exercise.name,
+                  order: exercise.order,
+                  sets: exercise.sets,
+                  reps: exercise.reps,
+                  restTimeInSeconds: exercise.restTimeInSeconds,
+                })),
+              },
+            })),
+          },
+        },
+      });
+      const result = await tx.workoutPlan.findUnique({
+        where: { id: workoutPlan.id },
+        include: {
+          workoutDays: {
+            include: {
+              exercises: true,
+            },
+          },
+        },
+      });
+      if (!result) {
+        throw new NotFoundError("Workout plan not found");
+      }
+
+      return result;
+    });
+  }
+}
+
+export { CreateWorkoutPlan };
